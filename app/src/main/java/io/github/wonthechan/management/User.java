803a0034624a720e857535fd7c6327f26e2b2abe
wonthechan <34850678+wonthechan@users.noreply.github.com>
2018-01-08 15:42:06 +0900

    add User class

---
+++ app/src/main/java/io/github/wonthechan/management/User.java
@@ -0,0 +1,52 @@
+package io.github.wonthechan.management;
+
+/**
+ * Created by YeChan on 2018-01-08.
+ */
+
+public class User {
+
+    String userID;
+    String userPassword;
+    String userName;
+    String userAge;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public User(String userID, String userPassword, String userName, String userAge) {
+        this.userID = userID;
+        this.userPassword = userPassword;
+        this.userName = userName;
+        this.userAge = userAge;
+    }
+}
