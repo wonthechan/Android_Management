091a954a7352eb43de2c8f3dd330c3bc06cb675a
wonthechan <34850678+wonthechan@users.noreply.github.com>
2018-01-08 15:39:04 +0900

    add DeleteRequest

---
+++ app/src/main/java/io/github/wonthechan/management/DeleteRequest.java
@@ -0,0 +1,30 @@
+package io.github.wonthechan.management;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by YeChan on 2018-01-08.
+ */
+
+public class DeleteRequest extends StringRequest {
+
+    final static private String URL = "http://211.252.53.222/Delete.php";
+    private Map<String, String> parameters;
+
+    public DeleteRequest(String userID, Response.Listener<String> listener){
+        super(Request.Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
